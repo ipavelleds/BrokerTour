86b732d00cba6ef1296dc8daa59d1163424d2609
znack <scher56@gmail.com>
2014-12-15 13:53:03 +0600

    add Proposal to admin

--- landing/admin.py
+++ landing/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from landing.models import Tour
+from landing.models import Tour, Proposal
+
+
+class ProposalAdmin(admin.ModelAdmin):
+    list_display = ('tour', 'phone', 'name', 'email')
 
 admin.site.register(Tour)
